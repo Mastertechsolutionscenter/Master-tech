c263a28b0d3c40bdd2f68a48c7918061429b8d99
Mastertechsolutionscenter <harmonyamanifoundation@gmail.com>
2026-05-06 13:46:18 +0300

    Update payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -73,7 +73,7 @@ export default buildConfig({
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
   db: mongooseAdapter({
-    url: process.env.DATABASE_URI || '',
+    url: process.env.MONGODB_URI || '',
   }),
   collections: [Pages, Posts, Media, Categories, Users, CaseStudies, Services, SubServices, FAQs, Leads, Projects],
   cors: [getServerSideURL()].filter(Boolean),
